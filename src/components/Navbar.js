63ef2a4d5857ff875a4a9c952790953905c67f02
Taranjeet Singh Dhingra <taranjeetsingh257@gmail.com>
2023-01-07 16:17:35 -0500

    removed comments

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,12 +21,6 @@ export default function Navbar(props) {
               <Link className="nav-link" to="/about">{props.aboutText}</Link>
             </li>
           </ul>
-          {/* Search button  */}
-          {/* <form className="d-flex" role="search">
-            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-            <button className="btn btn-primary" type="submit">Search</button>
-          </form> */}
-          {/* End Search Button  */}
         </div>
         <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
           <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
